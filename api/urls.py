c706a1cf34ca72a2705d8e7abf815b1235244ced
mochiyaki7 <kmomoda.0730@gmail.com>
2017-10-03 22:48:58 +0900

    (master)edit urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index/', include('index.urls')),
-    url(r'^bot/', include('bot.urls')),   
+    url(r'^index/', index.urls),
+    url(r'^bot/', bot.urls),   
 ]
